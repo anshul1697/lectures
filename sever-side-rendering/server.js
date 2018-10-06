b4c69dba6b5e61031c7e1af4065b9d50afba35d7
anshul1697 <anshul.r1610@gmail.com>
2018-10-06 17:21:14 +0530

    example

--- sever-side-rendering/server.js
+++ sever-side-rendering/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const srv = express();
-const todos =require('./routes/todoRoute')
+const todosRoute =require('./routes/todoRoute')
 
 srv.use(express.json());
 srv.use(express.urlencoded({extended: true}));
-srv.use('/todos',)
+srv.set('/todos',)
 
 srv.listen(8080, function(){
     console.log("Server started at 8080 port");
